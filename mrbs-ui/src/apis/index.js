b004691736bb62976f1b615c712c1cb948967f5d
JIANG WENSI <jiangwensi@hotmail.com>
2020-11-02 17:18:49 +0800

    update

--- mrbs-ui/src/apis/index.js
+++ mrbs-ui/src/apis/index.js
@@ -5,10 +5,10 @@ import axios from 'axios';
 // export const port="8080";
 // export const domain = "mrbs.jiangwensi.com";
 // prod aws elb
-// export const domain = "mrbs.jiangwensi.com/api";
+export const domain = "mrbs.jiangwensi.com/api";
 
 // dev docker
-export const domain = "192.168.99.100/api";
+// export const domain = "192.168.99.100/api";
 export const port = "";
 
 export default axios.create({
