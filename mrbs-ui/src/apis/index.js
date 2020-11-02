8b91dea035b278881bb64fd7246171c949cbd7bd
JIANG WENSI <jiangwensi@hotmail.com>
2020-11-02 12:49:24 +0800

    update

--- mrbs-ui/src/apis/index.js
+++ mrbs-ui/src/apis/index.js
@@ -4,10 +4,11 @@ import axios from 'axios';
 // export const domain = "192.168.99.100";
 // export const port="8080";
 export const domain = "mrbs.jiangwensi.com";
+export const apidomain = "api";
 export const port = "";
 
 export default axios.create({
-    baseURL: 'http://' + domain + port + '/',
+    baseURL: 'http://' + apidomain + port + '/',
     // headers: {
     // 	accept: 'application/json'
     // }
